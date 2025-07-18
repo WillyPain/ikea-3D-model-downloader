32991e369a740e7768b77f5b8e808138ca259420
WillPayne <payne.will@hotmail.com>
2025-07-19 03:50:18 +0800

    Added typescript models for node stuff. replaced https lib for nodes implementation of fetch. Made a more robust way for obtaining the glb url.

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,53 @@
 import { execFile } from 'child_process'
 import express from 'express';
 import path from 'path';
-import https from 'https';
 
 const app = express();
 const port = 3000;
 
+var xClientId: string | null | undefined;
+async function authorize(): Promise<string | undefined> {
+  var response = await fetch("https://www.ikea.com/global/assets/rotera/script-fragment-n2igtCEN.js");
+  var body = await response.text();
+  const match = body.match(/this\.modelBase="https:\/\/web-api\.ikea\.com",this\.key="(.*?)"/);
+  if (match?.[1]) {
+    return match[1];
+  }
+  return;
+}
 
-app.get('/download', (req, res) => {
+app.get('/download', (_, res) => {
   const formPath = path.resolve(__dirname, 'form.html');
   res.sendFile(formPath);
 });
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   const scriptPath = path.join(__dirname, '../src/download-glb.sh');
   const tempFile = path.join(__dirname, '../temp/downloaded.glb');
   const outputFile = path.join(__dirname, '../temp/output.glb');
 
-
   var url = req.query.url as string;
-  const filename = url.match(/[^/]+(?=\/?$)/) + ".glb";
+  const filename = url.match(/p\/(.*?)\//) + ".glb";
+  const productIdMatches = filename.match(/\d+/g);
+  const productId = productIdMatches?.[productIdMatches.length - 1] ?? "-1";
 
-  https.get(url, (response) => {
-    let body = '';
+  console.log(filename);
+  console.log(productId);
 
-    response.on('data', (chunk) => body += chunk);
-    response.on('end', () => {
-      const match = body.match(/https:\/\/[^\s"']+\.glb/g);
-      if (match?.[0]) {
-        url = match[0];
-        console.log(url);
-        execFile(scriptPath, [url, tempFile, outputFile], (error, stdout, stderr) => {
-          if (error) {
-            console.error('🚨 Error:', error);
-            return;
-          }
-          if (stderr) {
-            console.error('⚠️ stderr:', stderr);
-          }
-          console.log('✅ stdout:', stdout);
-          res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
-          res.sendFile(outputFile);
-        });
-      } else {
-        res.status(404).send('No .glb found'); 
-      }
-    });
-  }).on('error', (err) => {
-    res.status(500).send('Request error: ' + err.message);
+  xClientId = await authorize();
+  const  modelUrl = await getModelUrl(productId);
+
+  execFile(scriptPath, [modelUrl, tempFile, outputFile], (error, stdout, stderr) => {
+    if (error) {
+      console.error('🚨 Error:', error);
+      return;
+    }
+    if (stderr) {
+      console.error('⚠️ stderr:', stderr);
+    }
+    console.log('✅ stdout:', stdout);
+    res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
+    res.sendFile(outputFile);
   });
 });
 
@@ -59,3 +59,13 @@ app.listen(port, err => {
 });
 
 
+async function getModelUrl(modelId: string): Promise<string> {
+  const url = `https://web-api.ikea.com/au/en/rotera/data/model/${modelId}/`;
+  const headers: HeadersInit = new Headers();
+  headers.set("x-client-id", xClientId!);
+  
+  var response = await fetch(url, {headers: headers});
+  var ikeaObjectData = await response.json();
+
+  return ikeaObjectData.modelUrl;
+}
